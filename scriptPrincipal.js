82db68af1e837eb9f878bbd330f7a9230f43b324
gilberto barbato <gibarbato@gmail.com>
2024-02-25 12:05:53 -0300

    excluindoo function para quando fechar modal ir para home

--- scriptPrincipal.js
+++ scriptPrincipal.js
@@ -144,6 +144,7 @@ function shuffle(array) {
     return array;
 }
 
+
 function updateDateTime() {
     const currentDateTime = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
